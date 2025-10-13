866060c2b1bbf1d79d4bb91209c07d188d4dddfb
kin-chong <kinchong_low@yahoo.com>
2025-10-14 02:05:03 +0800

    Checkstyle fix

--- src/test/java/seedu/address/model/person/PersonContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/PersonContainsKeywordsPredicateTest.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import java.util.Arrays;
 import java.util.Collections;
 
