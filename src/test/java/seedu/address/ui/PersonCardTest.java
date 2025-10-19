20185f3890358807ddeba04ee70e3e920ce9a18e
Aiden Ma <52476917+mjykxz17@users.noreply.github.com>
2025-10-20 00:30:22 +0800

    Fix checkstyle

--- src/test/java/seedu/address/ui/PersonCardTest.java
+++ src/test/java/seedu/address/ui/PersonCardTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Field;
 import java.util.List;
-
 import javax.swing.SwingUtilities;
 
 import org.junit.jupiter.api.BeforeAll;
