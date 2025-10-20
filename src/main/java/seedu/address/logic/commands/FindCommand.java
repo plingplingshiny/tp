b5c18906d10d3f9e2b4c474e525ccddd34e2e7be
kin-chong <kinchong_low@yahoo.com>
2025-10-20 21:38:17 +0800

    Make changes according to comments

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -18,7 +18,7 @@ public class FindCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds persons by any combination of prefixes.\n"
             + "Parameters: [n/NAME_KEYWORDS] [p/PHONE_KEYWORDS] [e/EMAIL_KEYWORDS] [a/ADDRESS_KEYWORDS] "
             + "[t/TAG_KEYWORDS] [pr/PRICE_KEYWORDS] [pt/PROPERTY_TYPE_KEYWORDS] [i/INTENTION_KEYWORDS]\n"
-            + "At least one prefix is required. Keywords are case-insensitive and may contain spaces.\n"
+            + "At least one prefix is required. Keywords are case-insensitive.\n"
             + "Examples:\n"
             + "  find n/Alice p/9123\n"
             + "  find e/example.com a/Street t/friend\n"
